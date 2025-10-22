1428ff34766dce5f0f7bc74c66e5720778f04dd8
Rachel Ward <rward@derecho1.hsn.de.hpc.ucar.edu>
2025-10-22 15:29:34 -0600

    fix typo

--- main/FatesInterfaceMod.F90
+++ main/FatesInterfaceMod.F90
@@ -2426,7 +2426,7 @@ subroutine SeedlingParPatch(cpatch, &
 
   ! NEW CODE (RW FIXED)
   ! Start with the assumption that there is a single canopy layer
-  seedling_par_high = atm_par_dir+atm_par_dif
+  seedling_par_high = atm_par
   ! par_high_frac     = 1._r8-cpatch%total_canopy_area
   par_high_frac     = 1._r8 - min(1._r8, cpatch%total_canopy_area / cpatch%area)
   ! par_low_frac      = cpatch%total_canopy_area
